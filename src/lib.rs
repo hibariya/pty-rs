7b26ed326e2edf600610393e59e3943701497035
Hika Hibariya <hibariya@gmail.com>
2015-07-04 12:49:47 +0900

    Extract open_ptm and attach_pts

--- src/lib.rs
+++ src/lib.rs
@@ -12,36 +12,47 @@ macro_rules! unsafe_try {
 
 pub fn fork() -> io::Result<(libc::pid_t, libc::c_int)>
 {
-    let pty_master = unsafe_try!(ffi::posix_openpt(libc::O_RDWR));
-
-    unsafe_try!(ffi::grantpt(pty_master));
-    unsafe_try!(ffi::unlockpt(pty_master));
-
-    let pid = unsafe_try!(libc::fork());
+    let pty_master = try!(open_ptm());
+    let pid        = unsafe_try!(libc::fork());
 
     if pid == 0 {
-        let pts_name = unsafe { ffi::ptsname(pty_master) };
-
-        if (pts_name as i32) == 0 {
-            return Err(io::Error::last_os_error())
-        }
-
-        unsafe_try!(libc::close(pty_master));
-        unsafe_try!(libc::setsid());
-
-        let pty_slave = unsafe_try!(libc::open(pts_name, libc::O_RDWR, 0));
-
-        unsafe_try!(libc::dup2(pty_slave, libc::STDIN_FILENO));
-        unsafe_try!(libc::dup2(pty_slave, libc::STDOUT_FILENO));
-        unsafe_try!(libc::dup2(pty_slave, libc::STDERR_FILENO));
-
-        unsafe_try!(libc::close(pty_slave));
+        try!(attach_pts(pty_master));
 
         return Ok((0, -1));
     }
     else {
         return Ok((pid, pty_master));
     }
+}
+
+fn open_ptm() -> io::Result<libc::c_int> {
+    let pty_master = unsafe_try!(ffi::posix_openpt(libc::O_RDWR));
+
+    unsafe_try!(ffi::grantpt(pty_master));
+    unsafe_try!(ffi::unlockpt(pty_master));
+
+    Ok(pty_master)
+}
+
+fn attach_pts(pty_master: libc::c_int) -> io::Result<()> {
+    let pts_name = unsafe { ffi::ptsname(pty_master) };
+
+    if (pts_name as i32) == 0 {
+        return Err(io::Error::last_os_error())
+    }
+
+    unsafe_try!(libc::close(pty_master));
+    unsafe_try!(libc::setsid());
+
+    let pty_slave = unsafe_try!(libc::open(pts_name, libc::O_RDWR, 0));
+
+    unsafe_try!(libc::dup2(pty_slave, libc::STDIN_FILENO));
+    unsafe_try!(libc::dup2(pty_slave, libc::STDOUT_FILENO));
+    unsafe_try!(libc::dup2(pty_slave, libc::STDERR_FILENO));
+
+    unsafe_try!(libc::close(pty_slave));
+
+    Ok(())
 }
 
 #[inline]
