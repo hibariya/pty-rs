9148555b89218e8be63d99328e8caf9ed71b41e1
Hika Hibariya <hibariya@gmail.com>
2015-10-06 09:29:47 +0900

    Update documentation

---
+++ implementors/std/io/trait.Write.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['pty'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='pty/struct.ChildPTY.html' title='pty::ChildPTY'>ChildPTY</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
