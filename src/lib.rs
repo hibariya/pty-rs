f91b9c9a1c489d81b48f73a06cc98666d7632e5b
Hika Hibariya <hibariya@gmail.com>
2015-11-08 08:10:50 +0900

    Enable more lint options

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![deny(trivial_casts, trivial_numeric_casts,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
 #![cfg_attr(feature = "dev", allow(unstable_features))]
 #![cfg_attr(feature = "dev", feature(plugin))]
 #![cfg_attr(feature = "dev", plugin(clippy))]
@@ -84,7 +87,7 @@ impl Read for ChildPTY {
                        buf.len() as libc::size_t)
         }) {
             Ok(nread) => Ok(nread as usize),
-            Err(_) => Ok(0 as usize),
+            Err(_) => Ok(0),
         }
     }
 }
